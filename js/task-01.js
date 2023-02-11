944e934584b89a29202fe7190048f6335c97e272
Serhii Rudenko <svr.phoenix@gmail.com>
2023-02-11 10:33:51 +0100

    updating tasks 1, 5

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,8 @@
-const categoriesItemsRef = document
-  .querySelector("#categories")
-  .querySelectorAll(".item");
+const categoriesItemsRef = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${categoriesItemsRef.length}`);
 
 categoriesItemsRef.forEach(item => {
-  console.log(`Category: ${item.querySelector("h2").textContent}`);
-  console.log(`Elements: ${item.querySelectorAll("li").length}`);
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,12 @@
 const inputRef = document.querySelector("#name-input");
 const nameSpanRef = document.querySelector("#name-output");
+const initalInputValue = nameSpanRef.textContent;
 
-  const onInputChange = event => {
-    nameSpanRef.textContent = event.currentTarget.value.trim();
-  };
+const onInputChange = event => {
+  nameSpanRef.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    nameSpanRef.textContent = initalInputValue;
+  }
+};
 
 inputRef.addEventListener("input", onInputChange);
-
-
